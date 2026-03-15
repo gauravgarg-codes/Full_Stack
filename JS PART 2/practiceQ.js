668cc0347e6195e05da5e8bed9c08108b1bda519
Gaurav <Gauravgarg5@outlook.com>
2026-03-15 20:55:05 +0530

    DOM practice section

--- JS PART 2/practiceQ.js
+++ JS PART 2/practiceQ.js
@@ -23,5 +23,31 @@ for(let i=0; i < liss.length; i++){
 
 // TASK 03.
 // SELECT A PARAGRAPH AND REPLACE ITS CONTENT WITH:
-// 
+// <b> Updated </b> by javascript
+let p=document.querySelector("p");
+p.innerHTML = "<b>Updated</b> by javascript";
+
+// HOW TO DO YOU GET THE SRC OF AN IMAGE USING JAVASCRIPT
+let img = document.querySelector("img");
+console.log(img.src);
+//OR
+console.log(img.getAttribute("src"));
+
+// what does setAttribute() do?
+document.querySelector("img").setAttribute("src","https://images.unsplash.com/photo-1773088843510-277540282242?w=1000&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw3fHx8ZW58MHx8fHx8");
+
+// SELECT A LINK AND UPDATE ITS HREF TO POINT TO (https://shreyians.com)
+let a = document.querySelector("a");
+a.href = "https://www.shreyians.com"
+
+// add a tittle attribute to div dynamically
+let div = document.querySelector("div");
+div.setAttribute("tittle","some Info");
+
+// remove a disabled attribute from a button
+
+let btn = document.querySelector("button");
+btn.removeAttribute("disabled");
+
+// what does createElement() do? what's returned ?
 
